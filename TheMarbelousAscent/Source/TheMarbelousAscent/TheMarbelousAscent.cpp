dd5d95dcc4e6ecc24a28bbc7b70734df365e64a4
Chandler <cjoynes@umich.edu>
2026-02-13 19:42:19 -0500

    Added Unreal Engine Project

---
+++ TheMarbelousAscent/Source/TheMarbelousAscent/TheMarbelousAscent.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheMarbelousAscent.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheMarbelousAscent, "TheMarbelousAscent" );
